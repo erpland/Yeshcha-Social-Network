8de555be873e33fa7405ad576351900189a7b149
erpland <15Wineboim>
2022-05-13 18:38:39 +0300

    basic activities

--- app/src/main/java/com/example/final_project_semb/LoginActivity.java
+++ app/src/main/java/com/example/final_project_semb/LoginActivity.java
@@ -6,21 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Button login_btn;
     TextView toRegister_tv;
+    EditText email_et,password_et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login_btn = findViewById(R.id.btn_login);
-        toRegister_tv = findViewById(R.id.tv_toRegister);
+        initValues();
 
         login_btn.setOnClickListener(this);
         toRegister_tv.setOnClickListener(this);
+    }
+
+    private void initValues() {
+        login_btn = findViewById(R.id.btn_login);
+        toRegister_tv = findViewById(R.id.tv_toRegister);
+        email_et = findViewById(R.id.et_emailLogin);
+        password_et = findViewById(R.id.et_passwordLogin);
     }
 
     @Override
--- app/src/main/java/com/example/final_project_semb/RegisterActivity.java
+++ app/src/main/java/com/example/final_project_semb/RegisterActivity.java
@@ -2,13 +2,44 @@ package com.example.final_project_semb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    Button register_btn;
+    TextView toLogin_btn;
+    EditText email_et, password_et, passwordConfirm_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initValues();
+        toLogin_btn.setOnClickListener(this);
+        register_btn.setOnClickListener(this);
+    }
+
+    private void initValues() {
+        toLogin_btn = findViewById(R.id.tv_toLogin);
+        register_btn = findViewById(R.id.btn_register);
+        email_et = findViewById(R.id.et_emailRegister);
+        password_et = findViewById(R.id.et_passwordRegister);
+        passwordConfirm_et = findViewById(R.id.et_passwordAgain);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.tv_toLogin:
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                break;
+            case R.id.btn_register:
+                break;
+        }
     }
 }
