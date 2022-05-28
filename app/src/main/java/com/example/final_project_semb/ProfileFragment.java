3959906d41402088b1c2b6d24c8c4a0f11e37821
erpland <15Wineboim>
2022-05-29 02:10:56 +0300

    fixed profile picture upload and download and some design issues

--- app/src/main/java/com/example/final_project_semb/ProfileFragment.java
+++ app/src/main/java/com/example/final_project_semb/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.final_project_semb;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,12 +17,19 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class ProfileFragment extends Fragment {
     User user;
     ImageView profilePic;
     TextView name, phone;
     RatingBar stars;
     ViewGroup root;
+    StorageReference storageRef;
+    FirebaseStorage firebaseStorage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,10 +41,28 @@ public class ProfileFragment extends Fragment {
         phone.setText(user.getPhoneNumber());
         // TODO: 27/05/2022 ask roie what to do with profile picture-maybe async?
 //        profilePic.setImageURI(Uri.parse(user.getImage()));
+        getProfileImage();
         stars.setRating(user.getFlow_level());
         return root;
 
 
+    }
+    public void getProfileImage(){
+        StorageReference httpsReference = firebaseStorage.getReferenceFromUrl(user.image);
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        httpsReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                profilePic.setImageBitmap(Bitmap.createBitmap(bmp));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
     }
 
     private void initValues() {
@@ -42,6 +70,8 @@ public class ProfileFragment extends Fragment {
         name = root.findViewById(R.id.profile_name);
         phone = root.findViewById(R.id.profile_phone);
         stars = root.findViewById(R.id.rb_profileStars);
+        firebaseStorage = FirebaseStorage.getInstance();;
+        storageRef = firebaseStorage.getReference();
 
     }
 
--- app/src/main/java/com/example/final_project_semb/RegisterActivity.java
+++ app/src/main/java/com/example/final_project_semb/RegisterActivity.java
@@ -5,7 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -14,6 +16,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -27,17 +30,17 @@ import com.google.firebase.storage.UploadTask;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener,CallBackInterface {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, CallBackInterface {
 
     TextView toLogin_btn;
 
-    String emailTxt,passwordTxt,nameTxt,phoneNumberTxt;
-    private final static int GALLERY_PHOTO=99;
+    String emailTxt, passwordTxt, nameTxt, phoneNumberTxt;
+    private final static int GALLERY_PHOTO = 99;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
     FirebaseStorage firebaseStorage;
     StorageReference storageReference;
-    Uri imagePath;
+    Uri imagePath,imageUri;
     FragmentManager fragmentManager;
 
     @Override
@@ -47,55 +50,51 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         initValues();
         toLogin_btn.setOnClickListener(this);
-//        register_btn.setOnClickListener(this);
     }
 
     private void initValues() {
         toLogin_btn = findViewById(R.id.tv_toLogin);
-            mAuth=FirebaseAuth.getInstance();
-            db=FirebaseFirestore.getInstance();
-            firebaseStorage=FirebaseStorage.getInstance();
-            storageReference=firebaseStorage.getReference("Users");
-
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference("Users");
 
 
     }
 
-//    @Override
+    //    @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.tv_toLogin:
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 break;
         }
     }
 
-    public void callBackReg1(int viewId,String email,String password,String confirmPass){
-        emailTxt=email;
-        passwordTxt=password;
-        switch (viewId){
+    public void callBackReg1(int viewId, String email, String password, String confirmPass) {
+        emailTxt = email;
+        passwordTxt = password;
+        switch (viewId) {
             case R.id.btn_register1:
-                if (email.isEmpty()||password.isEmpty()||confirmPass.isEmpty())
-                {
-                    Toast.makeText(this,"אחד או יותר מן השדות ריקים ",Toast.LENGTH_LONG).show();
+                if (email.isEmpty() || password.isEmpty() || confirmPass.isEmpty()) {
+                    Toast.makeText(this, "אחד או יותר מן השדות ריקים ", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!password.equals(confirmPass)){
-                    Toast.makeText(this,"סיסמאות אינן תואמות!",Toast.LENGTH_LONG).show();
+                if (!password.equals(confirmPass)) {
+                    Toast.makeText(this, "סיסמאות אינן תואמות!", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!fieldValidation(email,Patterns.EMAIL_ADDRESS.toString())){
-                    Toast.makeText(this,"אימייל לא תקין",Toast.LENGTH_LONG).show();
+                if (!fieldValidation(email, Patterns.EMAIL_ADDRESS.toString())) {
+                    Toast.makeText(this, "אימייל לא תקין", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!fieldValidation(password,"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!-_$%^&+=])(?=\\S+$).{4,}$")) {
-                    Toast.makeText(this,"סיסמא נדרשת להכיל לפחות אות אחת גדולה,אות קטנה, מספר,סימן מיוחד",Toast.LENGTH_LONG).show();
+                if (!fieldValidation(password, "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!-_$%^&+=])(?=\\S+$).{4,}$")) {
+                    Toast.makeText(this, "סיסמא נדרשת להכיל לפחות אות אחת גדולה,אות קטנה, מספר,סימן מיוחד", Toast.LENGTH_LONG).show();
                     return;
-                }
-                else {
+                } else {
                     fragmentManager = getSupportFragmentManager();
                     fragmentManager.beginTransaction()
-                            .setCustomAnimations(R.anim.slide_to_right,R.anim.slide_to_left,R.anim.slide_from_right,R.anim.slide_from_left)
+                            .setCustomAnimations(R.anim.slide_to_right, R.anim.slide_to_left, R.anim.slide_from_right, R.anim.slide_from_left)
                             .replace(R.id.frgCntr_fregments, RegisterPage2Fragment.class, null)
                             .setReorderingAllowed(true)
                             .addToBackStack("f1") // name can be null
@@ -108,37 +107,38 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void callBackReg2(int viewId, String name, String phoneNumber, Uri image) {
-        nameTxt=name;
-        phoneNumberTxt=phoneNumber;
-        switch (viewId){
+        nameTxt = name;
+        phoneNumberTxt = phoneNumber;
+        switch (viewId) {
             case R.id.btn_registerFinish:
-                if (name.isEmpty()||phoneNumber.isEmpty())
-                {
-                    Toast.makeText(this,"אחד או יותר מן השדות ריקים ",Toast.LENGTH_LONG).show();
+                if (name.isEmpty() || phoneNumber.isEmpty()) {
+                    Toast.makeText(this, "אחד או יותר מן השדות ריקים ", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!fieldValidation(name,"^[a-zA-Z\\u0590-\\u05FF\\u200f\\u200e ]+$")){
-                    Toast.makeText(this,"שם יכול להכין אותיות בעברית/אנגלית בלבד",Toast.LENGTH_LONG).show();
+                if (!fieldValidation(name, "^[a-zA-Z\\u0590-\\u05FF\\u200f\\u200e ]+$")) {
+                    Toast.makeText(this, "שם יכול להכין אותיות בעברית/אנגלית בלבד", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (!fieldValidation(phoneNumber,"^(?:(?:(\\+?972|\\(\\+?972\\)|\\+?\\(972\\))(?:\\s|\\.|-)?([1-9]\\d?))|(0[23489]{1})|(0[57]{1}[0-9]))(?:\\s|\\.|-)?([^0\\D]{1}\\d{2}(?:\\s|\\.|-)?\\d{4})$")) {
-                    Toast.makeText(this,"מספר טלפון לא תקין",Toast.LENGTH_LONG).show();
+                if (!fieldValidation(phoneNumber, "^(?:(?:(\\+?972|\\(\\+?972\\)|\\+?\\(972\\))(?:\\s|\\.|-)?([1-9]\\d?))|(0[23489]{1})|(0[57]{1}[0-9]))(?:\\s|\\.|-)?([^0\\D]{1}\\d{2}(?:\\s|\\.|-)?\\d{4})$")) {
+                    Toast.makeText(this, "מספר טלפון לא תקין", Toast.LENGTH_LONG).show();
                     return;
                 }
                 break;
         }
 
-
-        User user=new User(emailTxt,null,nameTxt,phoneNumberTxt);
-        Map<String,Object>preferences=new HashMap<>();
-        Reply reply=new Reply();
-        Requests requests=new Requests();
+        User user = new User(emailTxt, null, nameTxt, phoneNumberTxt);
+        Map<String, Object> preferences = new HashMap<>();
+        Reply reply = new Reply();
+        Requests requests = new Requests();
         createPreferencesList(preferences);
-        SignUpToFireBase(emailTxt,passwordTxt,user,preferences,reply,requests,image);
 
+        if (image == null){ // אם לא העלו תמנו נעלה תמונה דיפולטיבית
+            Resources resources = this.getResources();
+            int resId = R.drawable.default_avatar;
+            image = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + resources.getResourcePackageName(resId) + '/' + resources.getResourceTypeName(resId) + '/' + resources.getResourceEntryName(resId) );
 
-
-
+        }
+        SignUpToFireBase(emailTxt, passwordTxt, user, preferences, reply, requests, image);
 
     }
 
@@ -147,53 +147,76 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         takeGalleryAction();
     }
 
-    private void createPreferencesList(@NonNull Map<String,Object> preferences) {
-        Preference basicEqt=new Preference(true,0,"Basics");
-        Preference computerMobilEqt=new Preference(true,1,"Computers and mobile");
-        Preference officeEqt=new Preference(true,2,"Office");
-        Preference othersEqt=new Preference(true,3,"Others");
-        Preference personalHygieneEqt=new Preference(true,4,"Personal hygiene");
-        Preference petEqt=new Preference(true,5,"Pets");
-        preferences.put("basicEqt",basicEqt);
-        preferences.put("computerMobilEqt",computerMobilEqt);
-        preferences.put("officeEqt",officeEqt);
-        preferences.put("othersEqt",othersEqt);
-        preferences.put("personalHygieneEqt",personalHygieneEqt);
-        preferences.put("petEqt",petEqt);
+    private void createPreferencesList(@NonNull Map<String, Object> preferences) {
+        Preference basicEqt = new Preference(true, 0, "Basics");
+        Preference computerMobilEqt = new Preference(true, 1, "Computers and mobile");
+        Preference officeEqt = new Preference(true, 2, "Office");
+        Preference othersEqt = new Preference(true, 3, "Others");
+        Preference personalHygieneEqt = new Preference(true, 4, "Personal hygiene");
+        Preference petEqt = new Preference(true, 5, "Pets");
+        preferences.put("basicEqt", basicEqt);
+        preferences.put("computerMobilEqt", computerMobilEqt);
+        preferences.put("officeEqt", officeEqt);
+        preferences.put("othersEqt", othersEqt);
+        preferences.put("personalHygieneEqt", personalHygieneEqt);
+        preferences.put("petEqt", petEqt);
     }
 
-    private void SignUpToFireBase(String email,String password,User user,Map<String,Object> preferences,Reply reply,Requests requests,Uri image){
+    private void SignUpToFireBase(String email, String password, User user, Map<String, Object> preferences, Reply reply, Requests requests, Uri image) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             //save user to the database
-                            UploadTask imageUpload=storageReference.child(mAuth.getUid()).child("Profile_"+System.currentTimeMillis()+".jpg").putFile(image);
+                            final StorageReference ref = storageReference.child(mAuth.getUid()).child("Profile_" + System.currentTimeMillis() + ".jpg");
+                            UploadTask imageUpload = ref.putFile(image);
 
-                            imageUpload.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            Task<Uri> urlTask = imageUpload.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                                 @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                    @Override
-                                    public void onSuccess(Uri uri) {
-                                        imagePath=taskSnapshot.getUploadSessionUri();
-
-
+                                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                    if (!task.isSuccessful()) {
+                                        throw task.getException();
                                     }
-                                });
+
+                                    // Continue with the task to get the download URL
+                                    return ref.getDownloadUrl();
+                                }
+                            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()) {
+                                        imageUri = task.getResult();
+                                        user.setImage(imageUri.toString());
+                                        addUserToFireStore(user, preferences, reply, requests);
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(RegisterActivity.this, "Authentication Success.",
+                                                Toast.LENGTH_SHORT).show();
+
+
+                                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Image Could Not Be Uploaded...",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             });
+//                            imageUpload.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                                @Override
+//                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                                    storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                                        @Override
+//                                        public void onSuccess(Uri uri) {
+//
+//                                            imagePath = taskSnapshot.getUploadSessionUri();
+//
+//
+//                                        }
+//                                    });
+//                                }
+//                            });
 
-                            user.setImage(imagePath.toString());
-                            addUserToFireStore(user,preferences,reply,requests);
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(RegisterActivity.this, "Authentication Success.",
-                                    Toast.LENGTH_SHORT).show();
 
-
-                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                         } else {
                             // If sign in fails, display a message to the user.
 
@@ -203,22 +226,21 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     }
                 });
     }
-    private boolean fieldValidation(String field,String regex){
-            if (!field.matches(regex)){
-                return false;
-            }
+
+    private boolean fieldValidation(String field, String regex) {
+        if (!field.matches(regex)) {
+            return false;
+        }
         return true;
     }
 
-    private void addUserToFireStore(User user,Map<String,Object> preferences,Reply reply,Requests requests){
+    private void addUserToFireStore(User user, Map<String, Object> preferences, Reply reply, Requests requests) {
         // Add a new document with a generated ID
         db.collection("users").document(mAuth.getUid())
                 .set(user);
         db.collection("Preferences").document(mAuth.getUid()).set(preferences);
         db.collection("Replies").document(mAuth.getUid()).set(reply);
         db.collection("Requests").document(mAuth.getUid()).set(requests);
-
-
 
 
     }
@@ -228,31 +250,26 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode){
+        switch (requestCode) {
 
             case GALLERY_PHOTO:
-                if (resultCode==RESULT_OK) {
-
-//
+                if (resultCode == RESULT_OK) {
                     imagePath = data.getData();
-
-                }
-                else{
-
-                    Toast.makeText(this,"Operation failed",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Operation failed", Toast.LENGTH_LONG).show();
                 }
                 break;
 
         }
     }
+
     //open gallery
-    private void takeGalleryAction(){
-        Intent pickPhoto=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(pickPhoto,GALLERY_PHOTO);
+    private void takeGalleryAction() {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto, GALLERY_PHOTO);
     }
 
-    public Uri getImageUri(){
-
+    public Uri getImageUri() {
         return imagePath;
     }
 
